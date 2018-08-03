fd8c35c91859ca52fc7cc58fac92221520995a94
Uditk18 <udit.karwasra18@gmail.com>
2018-08-04 02:09:53 +0530

    Employee assignment

---
+++ Employee Assignment/src/TestEmployee.java
@@ -0,0 +1,27 @@
+//main prog to test Employee.java
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+
+		Employee firstEmployee = new Employee();
+		System.out.println(firstEmployee);
+
+		firstEmployee.setId(111);
+		firstEmployee.setMonthlyBasic(17000.0);
+		firstEmployee.setName("Mayur");
+
+		System.out.println("Name: " + firstEmployee.getName());
+		System.out.println("id: " + firstEmployee.getId());
+		System.out.println("Basic Pay: " + firstEmployee.getMonthlyBasic());
+
+		System.out.println("Annual Basic: " + firstEmployee.getAnnualBasic());
+		System.out.println("Monthly Gross salary: " + firstEmployee.getMonthlyGrossSalary());
+		System.out.println("Annual Gross salary: " + firstEmployee.getAnnualGrossSalary());
+		System.out.println("Monthly deduction: " + firstEmployee.getMonthlyDeduction());
+		System.out.println("Monthly take home: " + firstEmployee.getMonthlyTakeHome());
+		System.out.println("Annual take home: " + firstEmployee.getAnnualTakeHome());
+
+	}
+
+}
